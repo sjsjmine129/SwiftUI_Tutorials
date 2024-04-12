3842608ef71f62224801ffbf8b514baea5098c11
sjsjmine129 <sjsjmine129@gmail.com>
2024-04-12 23:22:50 +0900

    4/12

--- Landmarks/Landmarks/Model/ModelData.swift
+++ Landmarks/Landmarks/Model/ModelData.swift
@@ -7,26 +7,31 @@
 
 import Foundation
 
-var landmarks: [Landmark] = load("landmarkData.json")
+@Observable
+class ModelData{
+    var landmarks: [Landmark] = load("landmarkData.json")
+}
 
-func load<T: Decodable>(_ filename: String)->T{
+
+func load<T: Decodable>(_ filename: String) -> T {
     let data: Data
-    
+
     guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
-    else{
-        fatalError("Couldn't find \(filename) in main bundle.")
+        else {
+            fatalError("Couldn't find \(filename) in main bundle.")
     }
-    
-    do{
+
+    do {
         data = try Data(contentsOf: file)
-    }catch{
+    } catch {
         fatalError("Couldn't load \(filename) from main bundle:\n\(error)")
     }
-    
-    do{
+
+    do {
         let decoder = JSONDecoder()
         return try decoder.decode(T.self, from: data)
-    }catch{
+    } catch {
         fatalError("Couldn't parse \(filename) as \(T.self):\n\(error)")
     }
 }
+
--- Landmarks/Landmarks/View/Landmarks/LandmarkDetail.swift
+++ Landmarks/Landmarks/View/Landmarks/LandmarkDetail.swift
@@ -8,9 +8,16 @@
 import SwiftUI
 
 struct LandmarkDetail: View {
+    @Environment(ModelData.self) var modelData
     var landmark: Landmark
     
+    var landmarkIndex: Int{
+        modelData.landmarks.firstIndex(where: { $0.id == landmark.id })!
+    }
+    
     var body: some View {
+        @Bindable var modelData = modelData
+        
         ScrollView{
             MapView(coordinate: landmark.locationCoordinate).frame(height: 300)
             
@@ -19,8 +26,13 @@ struct LandmarkDetail: View {
                 .padding(.bottom, -130)
             
             VStack(alignment: .leading){
-                Text(landmark.name)
-                    .font(.title)
+                
+                HStack {
+                    Text(landmark.name)
+                        .font(.title)
+                    
+                    FavoriteButton(isSet: $modelData.landmarks[landmarkIndex].isFavorite)
+                }
                 
                 HStack {
                     Text(landmark.park)
@@ -47,5 +59,6 @@ struct LandmarkDetail: View {
 }
 
 #Preview {
-    LandmarkDetail(landmark: ModelData().landmarks[0])
+    let modelData = ModelData()
+    return LandmarkDetail(landmark: ModelData().landmarks[0]).environment(modelData)
 }
